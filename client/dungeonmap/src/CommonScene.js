62ef87a047cb1dba422dc915f94940566a07329f
DonQuixuote <donquixuote@gmail.com>
2024-12-11 01:17:51 -0500

    Update CommonScene.js
    Moved game logic to server side.

--- client/dungeonmap/src/CommonScene.js
+++ client/dungeonmap/src/CommonScene.js
@@ -1,12 +1,13 @@
 import Phaser from "phaser";
 import Level from "./Level.js";
+import io from 'socket.io-client';
 
 export default class CommonScene extends Phaser.Scene {
   constructor() {
     super("CommonScene");
     this.player = null;
-    this.level = new Level();
-    this.spike = null;
+    this.otherPlayers = new Map();
+    this.socket = io('http://localhost:8080');
   }
 
   preload() {
@@ -101,46 +102,42 @@ export default class CommonScene extends Phaser.Scene {
         .setScale(1, 1)
         .setOrigin(0, 0)
         .setCollisionByProperty({ collider: true,  });
+     //create layer id for collsion
+ 
+    // Make sure player loads
+    console.log('Creating player sprite...');
+    this.player = this.add.sprite(400, 300, "player")
+        .setScale(1)
+        .setDepth(10)
+        .setVisible(true);
+    
+    console.log('Player created:', this.player);
+    console.log('Player position:', this.player.x, this.player.y);
+    console.log('Player visible:', this.player.visible);
 
-    // const tileset2 = map.addTilesetImage("pillars", "pillars");
-    // const layer3 = map.createLayer("pillars", tileset2, 0, 0);
-    // layer3.setScale(1, 1).setOrigin(0, 0);
-    // layer3.setCollisionByProperty({ collider: true });
+    this.createAnimations();
 
-    this.player = this.physics.add
-        .sprite(
-            this.game.config.width /2,
-            this.game.config.height/2,
-            "player",
-        )
-        .setScale(1);
+    this.cameras.main.startFollow(this.player, true);
+    this.cameras.main.setFollowOffset(-50, -50);
 
-    this.player.setScale(1); // Scale the player sprite by 1.5 times
-    this.player.setBodySize(24, 28);
-    this.player.setOffset(10, 13);
+    this.socket.emit('joinGame', { 
+        scene: 'common',
+        x: this.player.x,
+        y: this.player.y 
+    });
+
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.setupSocketListeners();
+  }
+
+  createAnimations() {
     this.anims.create({
       key: "idleDown",
       frames: this.anims.generateFrameNumbers("player", { start: 0, end: 5 }),
       frameRate: 10,
       repeat: -1,
     });
-
-    this.physics.add.collider(this.player, layer1, );
-    this.physics.add.collider(this.player, objectLayer, (a, b)=>{
-     if(b?.properties?.dungeon){
-       this.scene.start('DungeonScene')
-     }
-      if(b?.properties?.bridge){
-        this.scene.start('BridgeScene')
-      }
-
-    });
-
-
-    // this.cameras.main.setBounds(0, 0, +this.game.config.width, +this.game.config.height);
-    this.cameras.main.startFollow(this.player, true)
-    this.cameras.main.setFollowOffset(-50,-50)
 
     this.anims.create({
       key: "idleRight",
@@ -207,60 +204,62 @@ export default class CommonScene extends Phaser.Scene {
     this.player.play("idleDown");
   }
 
-  update() {
-    const speed = 80;
-    const prevVelocity = this.player.body.velocity.clone();
-    let newX = this.player.x;
-    let newY = this.player.y;
+  setupSocketListeners() {
+    this.socket.on('currentPlayers', (players) => {
+        Object.keys(players).forEach((id) => {
+            if (id === this.socket.id) {
+                // Update our player position from server
+                this.updatePlayerState(this.player, players[id]);
+                // Set camera to follow our player
+                this.cameras.main.startFollow(this.player, true);
+                this.cameras.main.setFollowOffset(-50, -50);
+            } else {
+                this.addOtherPlayer(players[id]);
+            }
+        });
+    });
 
-    // Stop any previous movement from the last frame
-    this.player.body.setVelocity(0);
-
-    // Horizontal movement
-    if (this.cursors.left.isDown) {
-      this.player.body.setVelocityX(-speed);
-      this.player.anims.play("walkRight", true); // Assuming you have a 'walkLeft' animation
-      this.player.flipX = true; // Flip the sprite to face left
-    } else if (this.cursors.right.isDown) {
-      this.player.body.setVelocityX(speed);
-      this.player.anims.play("walkRight", true);
-      this.player.flipX = false; // Ensure the sprite is facing right
-    }
-
-    // Vertical movement
-    if (this.cursors.up.isDown) {
-      this.player.body.setVelocityY(-speed);
-      this.player.anims.play("walkUp", true);
-    } else if (this.cursors.down.isDown) {
-      console.log("not");
-      this.player.body.setVelocityY(speed);
-      this.player.anims.play("walkDown", true);
-    }
-
-    // Normalize and scale the velocity so that player can't move faster along a diagonal
-    this.player.body.velocity.normalize().scale(speed);
-
-    // If no movement keys are pressed, stop the animation
-    if (
-        this.cursors.left.isUp &&
-        this.cursors.right.isUp &&
-        this.cursors.up.isUp &&
-        this.cursors.down.isUp
-    ) {
-      this.player.anims.stop();
-
-      // Set idle animation based on the last direction
-      if (prevVelocity.x < 0) {
-        this.player.anims.play("idleRight", true);
-        this.player.flipX = true;
-      } else if (prevVelocity.x > 0) {
-        this.player.anims.play("idleRight", true);
-        this.player.flipX = false;
-      } else if (prevVelocity.y < 0) {
-        this.player.anims.play("idleUp", true);
-      } else if (prevVelocity.y > 0) {
-        this.player.anims.play("idleDown", true);
-      }
-    }
-  }
+    // ... rest of the listeners remain the same ...
 }
+
+sendCollisionData() {
+    // collisions
+    const collisionMap = [];
+    const mapHeight = this.layer1.layer.height;
+    const mapWidth = this.layer1.layer.width;
+
+    for (let y = 0; y < mapHeight; y++) {
+        collisionMap[y] = [];
+        for (let x = 0; x < mapWidth; x++) {
+            const tile1 = this.layer1.getTileAt(x, y);
+            const tileObject = this.objectLayer.getTileAt(x, y);
+            collisionMap[y][x] = (tile1 && tile1.properties.collider) || 
+                                (tileObject && tileObject.properties.collider) || 
+                                false;
+        }
+    }
+
+    this.socket.emit('sceneCollision', {
+        scene: 'common',
+        collisionMap: collisionMap
+    })
+}
+// receive input -- server / check collsion = revert 
+update() {
+    if (!this.player) return;
+
+    this.socket.emit('playerInput', {
+        left: this.cursors.left.isDown,
+        right: this.cursors.right.isDown,
+        up: this.cursors.up.isDown,
+        down: this.cursors.down.isDown,
+        scene: 'common'
+    });
+}
+
+// Disconnect when scene is shut down
+shutdown() {
+    if (this.socket) {
+        this.socket.removeAllListeners();
+ 
+}}}
