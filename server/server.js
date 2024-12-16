d27be2a0c581800a700ffd85c0a07e75777a3dd6
Ayush <ayushagarwal.dev@gmail.com>
2024-12-16 17:57:33 +0530

    add vercel config

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express()
 const httpServer = http.createServer(app)
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+    origin: ['*'],
     credentials: false,
   },
 })
