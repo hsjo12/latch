49dd25a2de0cf8df0c89886c255b14a80431ed76
hsjo12 <hsjo12@naver.com>
2024-12-17 12:44:51 +0900

    fix

--- client/website/src/utils/provider/provider.js
+++ client/website/src/utils/provider/provider.js
@@ -2,12 +2,12 @@ import { ethers } from "ethers";
 import { networkInfo } from "./data";
 
 export const getProvider = (chainId = 360) => {
-  const rpc = networkInfo[chainId].rpc;
-  return new ethers.JsonRpcProvider(rpc);
+  // const rpc = networkInfo[chainId].rpc;
+  return new ethers.JsonRpcProvider("https://mainnet.shape.network");
 };
 
 export const getContract = (target, abi, chainId) => {
-  const rpc = networkInfo[chainId].rpc;
-  const provider = new ethers.JsonRpcProvider(rpc);
+  // const rpc = networkInfo[chainId].rpc;
+  const provider = new ethers.JsonRpcProvider("https://mainnet.shape.network");
   return new ethers.Contract(target, abi, provider);
 };
